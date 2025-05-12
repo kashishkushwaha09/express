36bf7dd9a67b5c7a2d4eaf72b90d7f43e579cfd4
kashishkushwaha09 <kashishkushwaha03@gmail.com>
2025-05-12 20:12:15 +0530

    e-commerce project

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ server.use((req,res,next)=>{
 server.use('/welcome',(req,res,next)=>{
     req.user='Guest'
      next();
-})
-server.get('/welcome',(req,res)=>{
+},(req,res)=>{
     res.send(`<h1>Welcome, ${req.user}!</h1>`)
 })
 server.get('/library2',(req,res)=>{
@@ -17,6 +16,7 @@ server.get('/library2',(req,res)=>{
 })
 
 //Question-Routes
+
 server.get('/users',(req,res)=>{
      res.send('<h1>Here is the list of all users.</h1>')
 })
@@ -29,6 +29,22 @@ server.post('/users',(req,res)=>{
 server.post('/orders',(req,res)=>{
      res.send('<h1>A new order has been created.</h1>')
 })
+// e-commerce routes 
+server.get('/products',(req,res)=>{
+    res.send('<h1>Here is the list of all products.</h1>');
+})
+server.post('/products',(req,res)=>{
+    res.send('<h1>A new product has been added.</h1>');
+})
+server.get('/categories',(req,res)=>{
+    res.send('<h1>Here is the list of all categories.</h1>');
+})
+server.post('/categories',(req,res)=>{
+    res.send('<h1>A new category has been added.</h1>');
+})
+server.use((req, res) => {
+    res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
 server.listen(port,()=>{
     console.log(`Server is up and running on port ${port}! Ready to handle requests.`)
 })
